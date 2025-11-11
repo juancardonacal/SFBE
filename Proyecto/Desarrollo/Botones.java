0001bfbf214ce825a0f241fb2608336ca2974044
Juan Esteban Cardona <juan.cardonacal@campusucc.edu.co>
2025-11-11 04:49:44 +0000

    Botones en desarrollo

---
+++ Proyecto/Desarrollo/Botones.java
@@ -0,0 +1,39 @@
+public class Botones {
+
+    protected boolean presionado;
+    protected boolean Luz;
+    protected boolean sonido;
+
+    public Botones(boolean presionado, boolean luz, boolean sonido) {
+        this.presionado = false;
+        this.Luz = false;
+        this.sonido = false;
+    }
+
+    public void iluminar() {
+        if (Luz == false) {
+            Luz = true;
+            System.out.println("Luz encendida");
+        } else {
+            Luz = false;
+            System.out.println("Luz apagada");
+    }
+
+
+    }
+
+    public void activarSonido() {
+        if (sonido == false) {
+            if (presionado == true) {
+                System.out.println("boton presionado, sonar");
+                return;
+            }
+            sonido = true;
+            System.out.println("Sonido activado");
+        } else {
+            sonido = false;
+            System.out.println("Sonido desactivado");
+        }
+    }
+
+}
---
+++ Proyecto/Desarrollo/botonesPiso.java
@@ -0,0 +1,13 @@
+public class BotonesPiso extends Botones {
+
+    public void subir() {
+        direccion = "arriba";
+        Luz = true;
+        sonido = true;
+    }
+
+    public void bajar() {
+        direccion = "abajo";
+        Luz = true;
+        sonido = true;
+    }
